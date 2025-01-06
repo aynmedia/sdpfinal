ef801be6dcbbb140eca63f564cb678074668ffa6
Juddan <juddan2008@gmail.com>
2025-01-06 18:58:28 +0530

    first commit

--- src/app/components/herosection/herosection.jsx
+++ src/app/components/herosection/herosection.jsx
@@ -5,28 +5,33 @@ import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Button from '../ui/button/button';
-import sliderbg1 from '@/../public/images/sliderbg1.jpg';
+import herohome from '@/../public/images/herohome.jpg';
 import purplebg from '@/../public/images/purplebg.jpg';
 const Herosection = () => {
   return (
-    <div className='relative w-full h-[90vh] overflow-hidden'>
-      <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#111016] via-transparent to-[#111016] z-10 pointer-events-none'></div>
+    <div className='relative w-full h-[100vh] overflow-hidden'>
       {/* Background Image */}
-
+      <Image
+        src={herohome}
+        alt='Slider Background'
+        layout='fill'
+        objectFit='cover'
+        className='absolute top-0 left-0 w-full h-full'
+      />
       {/* <div className='absolute inset-0 top-0 bg-gradient-to-b from-black/70 to-transparent'></div> */}
 
       {/* Overlay Structure - Modified to 2 columns */}
       <div className='absolute inset-0 grid md:grid-cols-2 gap-0'>
         {/* Left colored panel */}
         <motion.div
-          className='col-span-1 bg-[#111016]'
+          className='col-span-1 '
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.6 }}
         />
         {/* Right colored panel */}
         <motion.div
-          className='col-span-1 bg-[#111016]/90'
+          className='col-span-1 '
           initial={{ x: 100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.6 }}
@@ -42,11 +47,11 @@ const Herosection = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.3 }}>
           <motion.h1
-            className='text-white text-4xl md:text-4xl font-bold'
+            className='text-white text-4xl md:text-6xl font-bold font-oswald'
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.5 }}>
-            Efficient Pneumatic Solutions
+            Precision Engineering, Redefined
           </motion.h1>
 
           <motion.p
@@ -54,8 +59,10 @@ const Herosection = () => {
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.7 }}>
-            We deliver cutting-edge pneumatic systems designed to enhance
-            productivity and reliability.
+            Step into a world of innovation and reliability with tools and
+            solutions designed to empower your projects. From cutting-edge
+            technology to unmatched craftsmanship, experience engineering that
+            redefines precision and elevates performance at every level.
           </motion.p>
 
           <motion.div
@@ -72,19 +79,10 @@ const Herosection = () => {
 
         {/* Right Content */}
         <motion.div
-          className='flex flex-col justify-center bg-white col-span-1 relative'
+          className='flex flex-col justify-center e col-span-1 relative'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.3 }}>
-          <Image
-            src={purplebg}
-            alt='Slider Background'
-            layout='fill'
-            objectFit='cover'
-            className='absolute top-0 left-0 w-full h-full'
-          />
-          <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#111016] via-transparent to-[#111016] z-10 pointer-events-none'></div>
-        </motion.div>
+          transition={{ duration: 0.5, delay: 0.3 }}></motion.div>
       </div>
     </div>
   );
